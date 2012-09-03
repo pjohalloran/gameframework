6eb93efdbfae5fd8255a2641baa39d8ad273751a
pjohalloran <pjohalloran@gmail.com>
2012-09-03 16:48:42 +0100

    Adding script to install dependant 3rd party libs using Macports.

---
+++ src/build/macosx/3rdPartyInstall.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+#
+# 3rdPartyInstall
+# PJ O Halloran
+# 03/09/2012
+#
+# Install all dependant 3rdParty libs using Macports.
+#
+# Please install and/or update to the latest version of Macports before running this script.
+#
+
+TRUE=0
+FALSE=1
+
+VERBOSE=FALSE
+
+main()
+{
+    if [ ! -e /opt/local/bin/port ]; then
+        echo "Please install and/or update to the latest version of macports!"
+        return $FALSE
+    fi
+    
+    sudo port install boost libvorbis vorbis-tools bullet ftgl glew jpeg libogg libpng
+    return $?
+}
+
+main $*
+if [ $? -ne $TRUE ]; then
+    echo $0 .. " failed to complete."
+fi
